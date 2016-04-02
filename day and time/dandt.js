bfefdc66dfdefe2a72f5fd3ceff19885a1da8884
Jonas <joshuamoorhead@gmail.com>
2016-04-02 14:00:02 -0700

    changes

--- day and time/dandt.js
+++ day and time/dandt.js
@@ -7,30 +7,24 @@ var minute = today.getMinutes();
 var second = today.getSeconds();
 var prepand = (hour >= 12)? " PM ":" AM ";
 hour = (hour >= 12)? hour-12: hour;
-if (hour===0 && prepand===' PM ')
-{
-if (minute===0 && second===0)
-{
-hour=12;
-prepand=' Noon';
+if (hour===0 && prepand===' PM '){
+	if (minute===0 && second===0){
+		hour=12;
+		prepand=' Noon';
+	}
+	else{
+		hour=12;
+		prepand=' PM';
+	}
 }
-else
-{
-hour=12;
-prepand=' PM';
-}
-}
-if (hour===0 && prepand===' AM ')
-{
-if (minute===0 && second===0)
-{
-hour=12;
-prepand=' Midnight';
-}
-else
-{
-hour=12;
-prepand=' AM';	
-}
+if (hour===0 && prepand===' AM '){
+	if (minute===0 && second===0){
+		hour=12;
+		prepand=' Midnight';
+	}
+	else{
+		hour=12;
+		prepand=' AM';	
+	}
 }
 console.log("Current Time : "+hour + prepand + " : " + minute + " : " + second);
