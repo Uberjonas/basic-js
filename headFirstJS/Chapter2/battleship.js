4b7e3cba4ec04d2bccff98c53a3b059c5aed9275
Jonas <joshuamoorhead@gmail.com>
2016-04-04 16:58:01 -0700

    fix code

--- headFirstJS/Chapter2/battleship.js
+++ headFirstJS/Chapter2/battleship.js
@@ -15,13 +15,12 @@ while(isSunk==false){
 		if(guess==location1||guess=location2||guess=location3){
 			alert("HIT!");
 			hits=hits+1;
-		}else{
-			alert("MISS!");
-		}
 			if(hits==3){
 				isSunk=true;
 				alert("You sank my battleship!");
 			}
+		}else{
+			alert("MISS!");
 		}
 	}
 }
