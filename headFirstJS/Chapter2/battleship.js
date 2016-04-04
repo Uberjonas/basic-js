bbfd161515a11f46f55c1ff24732a41655da64f0
Jonas <joshuamoorhead@gmail.com>
2016-04-04 16:50:56 -0700

    more code

--- headFirstJS/Chapter2/battleship.js
+++ headFirstJS/Chapter2/battleship.js
@@ -12,5 +12,14 @@ while(isSunk==false){
 		alert("Enter a valid cell number, freakshow.");
 	}else{
 		guesses=guesses+1;
+		if(guess==location1||guess=location2||guess=location3){
+			hits=hits+1;
+			if(hits==3){
+				isSunk=true;
+				alert("You sank my battleship!");
+			}
+		}
 	}
 }
+var stats = "You took "+guesses+" guesses to sink the battleship, "+"which means your shooting accuracy was "+(3/guesses);
+alert(stats);
