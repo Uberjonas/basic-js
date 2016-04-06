200def0295e426580c990b097570190e5d3e5059
Jonas <joshuamoorhead@gmail.com>
2016-04-06 10:43:44 -0700

    js edit

--- headFirstJS/Chapter3/functions.js
+++ headFirstJS/Chapter3/functions.js
@@ -2,12 +2,12 @@ function fuckFace(x,y){
 	var tool = x*x+y*y;
 	var homo = x/y+42;
 	var freakShow = 21;
-	if(x<y){
-		document.write(tool);
-	}else if(x>y){
-		document.write(homo);
+	if(x < y){
+		console.log(tool);
+	}else if(x > y){
+		console.log(homo);
 	}else{
-		document.write(freakShow*2);
+		console.log(freakShow*2);
 	}
 }
 fuckFace(2,5);
