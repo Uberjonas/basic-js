33b9077ba87ae1609cbfe18924405f5d65700e89
Jonas <joshuamoorhead@gmail.com>
2016-04-06 10:08:55 -0700

    added some js

---
+++ headFirstJS/Chapter3/functions.js
@@ -0,0 +1,12 @@
+function fuckFace(x,y){
+	if(x<y){
+		var tool = x*x+y*y;
+		document.write(tool);
+	}else if(x>y){
+		var homo = x/y+42;
+		document.write(homo);
+	}else{
+		var freakShow = 21;
+		document.write(freakShow*2);
+	}
+}
