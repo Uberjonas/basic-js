7d2bea4c322cf285ab67a16b654f1a64ca0e4f5a
Jonas <joshuamoorhead@gmail.com>
2016-04-08 11:36:40 -0700

    adding array js to ward story

--- headFirstJS/Chapter4/arrays.js
+++ headFirstJS/Chapter4/arrays.js
@@ -1,22 +1,29 @@
 function game(){
-	var answer1 = prompt("Ward is clearly gay, but how gay is he? Want to play a game and find out?(yes or no)");
+    var answer1 = prompt("Ward is clearly gay, but how gay is he? Want to play a game and find out?(yes or no)");
 	if(answer1 == "yes"){
 		var answer2 = prompt("That is correct. Step one. How many cocks are currently penetrating him?(enter a number tard)");
 		if (answer2 <= 1){
         	var answer3 = prompt("Based on what we know of Ward, clearly the number is higher. Try again.(Enter bigger number)");
         	if(answer3 <= 1){
-            alert("Fuck you, go away.");
+                alert("Fuck you, go away.");
             }
         }else{
-        	var win = answer2;
-        	var answer4 = prompt("You guessed "+win+" cocks! Let's take this further. If you add the cocks he's holding, how many is that?")
-        	var grip = 3;
-        	var cockAdd = answer4;
-        	var correct = (Number(win) + grip);
-          	console.log(cockAdd);
-          	console.log(correct);
+	        var win = answer2;
+	        var answer4 = prompt("You guessed "+win+" cocks! Let's take this further. If you add the cocks he's holding, how many is that?")
+	        var grip = 3;
+	        var cockAdd = answer4;
+	        var correct = (Number(win) + grip);
         	if(cockAdd == correct){              
-            	var answer5 = prompt("Holy shit...good guess. That's one in each hand, the penetrators, and the one between his feet.");
+            	var answer5 = prompt("Holy shit...good guess. That's one in each hand, the penetrators, and the one between his feet. Want to use this info to build an array?(yes/no).");
+                if(answer5 == "yes"){
+                    var cockSucker = [];
+                    for(var i=0;i<cockAdd;i++){
+                        cockSucker.push(Math.ceil(Math.random()*12));
+                    }
+                console.log(cockSucker);
+                }else{
+                    alert("Fine, dick, but this was gonna get pretty great.");
+                }
             }else{
           	  	alert("Nerp, your game ends here. You were close though.You should try again.");
             }
