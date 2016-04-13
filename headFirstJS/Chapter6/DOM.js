a2e1486007b732092c17ee658996f0b8eeaf9755
Jonas <joshuamoorhead@gmail.com>
2016-04-13 11:07:01 -0700

    testing style

--- headFirstJS/Chapter6/DOM.js
+++ headFirstJS/Chapter6/DOM.js
@@ -1,5 +1,6 @@
 function init(){
 	var planet = document.getElementById("greenplanet");
 	planet.innerHTML = "Red Alert: Hit by phaser fire!";
+	planet.setAttribute("class","redtext");
 }
 window.onload = init;
