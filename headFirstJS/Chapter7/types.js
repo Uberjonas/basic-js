5220523c2eb5637592ab77f711c3adf6d120cb02
Jonas <joshuamoorhead@gmail.com>
2016-04-13 15:46:06 -0700

    js example

---
+++ headFirstJS/Chapter7/types.js
@@ -0,0 +1,9 @@
+function Duck(sound){
+  this.sound = sound;
+  this.quack = function() {console.log(this.sound);}
+}
+var toy = new Duck("quack quack");
+toy.quack();
+
+console.log(typeof toy);
+console.log(toy instanceof Duck);
