ab7c218631b97c06d087140bc2cbce148cb68441
Jonas <joshuamoorhead@gmail.com>
2016-04-14 16:13:07 -0700

    fuckin battleship

--- headFirstJS/Chapter8/app.js
+++ headFirstJS/Chapter8/app.js
@@ -82,6 +82,17 @@ function parseGuess(guess){
 	}
 	return null;
 };
+function init(){
+	var fireButton = document.getElementById("fireButton");
+	fireButton.onclick = handleFireButton;
+}
+function handleFireButton(){
+	var guessInput = document.getElementById("guessInput");
+	var guess = guessInput.value;
+	controller.processGuess(guess);
+	guessInput.value = "";
+}
+window.onload = init;
 controller.processGuess("A0");
 controller.processGuess("A6");
 controller.processGuess("B6");
