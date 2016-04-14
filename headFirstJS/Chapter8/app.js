5573e38495dcdc1b44f6920e78a3f8665bdfeadd
Jonas <joshuamoorhead@gmail.com>
2016-04-14 09:01:06 -0700

    adding message js

---
+++ headFirstJS/Chapter8/app.js
@@ -0,0 +1,12 @@
+var view = {
+	displayMessage: function(msg){
+		var messageArea = document.getElementById("messageArea");
+		messageArea.innerHTML = msg;
+	},
+	displayHit: function(){
+
+	},
+	displayMiss: function(){
+
+	}
+};
