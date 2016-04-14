06d0896879649dd758736c55d873741c65a295b6
Jonas <joshuamoorhead@gmail.com>
2016-04-14 15:40:32 -0700

    controller test

--- headFirstJS/Chapter8/app.js
+++ headFirstJS/Chapter8/app.js
@@ -53,7 +53,14 @@ var controller = {
 	guesses: 0,
 
 	processGuess: function(guess){
-
+		var location = parseGuess(guess);
+		if (location){
+			this.guesses++;
+			var hit = model.fire(location);
+			if (hit && model.shipsSunk === model.numShips){
+				view.displayMessage("You sank all my battleships in " + this.guesses + " guesses.");
+			}
+		}
 	}
 };
 function parseGuess(guess){
@@ -75,8 +82,13 @@ function parseGuess(guess){
 	}
 	return null;
 };
-console.log(parseGuess("A0"));
-console.log(parseGuess("B6"));
-console.log(parseGuess("G3"));
-console.log(parseGuess("H0"));
-console.log(parseGuess("A7"));
+controller.processGuess("A0");
+controller.processGuess("A6");
+controller.processGuess("B6");
+controller.processGuess("C6");
+controller.processGuess("C4");
+controller.processGuess("D4");
+controller.processGuess("E4");
+controller.processGuess("B0");
+controller.processGuess("B1");
+controller.processGuess("B2");
