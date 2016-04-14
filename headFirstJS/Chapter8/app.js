036fcd2d11dafc4b071b8495fa699d623dd17dfb
Jonas <joshuamoorhead@gmail.com>
2016-04-14 10:26:56 -0700

    added hits to model

--- headFirstJS/Chapter8/app.js
+++ headFirstJS/Chapter8/app.js
@@ -20,7 +20,19 @@ var model = {
 
 	ships: [{ locations: ["06","16","26"], hits: ["","",""] },
 					{ locations: ["24","34","44"], hits: ["","",""] },
-					{ locations: ["10","11","12"], hits: ["","",""] }]
+					{ locations: ["10","11","12"], hits: ["","",""] }],
+	fire: function(guess){
+		for (var i=0; i<this.numShips; i++){
+			var ship = this.ships[i];
+			var locations = ship.locations;
+			var index = locations.indexOf(guess);
+			if (index >= 0){
+				ship.hits[index] = "hit";
+				return true;
+			}
+			return false;
+		}
+	}
 };
 view.displayMiss("00");
 view.displayHit("34");
