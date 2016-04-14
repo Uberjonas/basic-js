8fa3bdd60218e4a95f2c77aaf4bc76b08685439b
Jonas <joshuamoorhead@gmail.com>
2016-04-14 09:27:29 -0700

    test run

--- headFirstJS/Chapter8/app.js
+++ headFirstJS/Chapter8/app.js
@@ -12,3 +12,9 @@ var view = {
 		cell.setAttribute("class", "miss");
 	}
 };
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
