fd167d666792db8d246acb8af2a86240264ae983
Jonas <joshuamoorhead@gmail.com>
2016-04-14 10:58:11 -0700

    model test

--- headFirstJS/Chapter8/app.js
+++ headFirstJS/Chapter8/app.js
@@ -49,10 +49,13 @@ var model = {
 		return true;
 	}
 };
-view.displayMiss("00");
-view.displayHit("34");
-view.displayMiss("55");
-view.displayHit("12");
-view.displayMiss("25");
-view.displayHit("26");
-view.displayMessage("fucknuts");
+model.fire("53");
+model.fire("06");
+model.fire("16");
+model.fire("26");
+model.fire("34");
+model.fire("24");
+model.fire("44");
+model.fire("12");
+model.fire("11");
+model.fire("10");
