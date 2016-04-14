c9c425dae6b0af5e82db050b82e42d607adb79f7
Jonas <joshuamoorhead@gmail.com>
2016-04-14 16:14:36 -0700

    gotta erase the test guesses idiot

--- headFirstJS/Chapter8/app.js
+++ headFirstJS/Chapter8/app.js
@@ -93,13 +93,3 @@ function handleFireButton(){
 	guessInput.value = "";
 }
 window.onload = init;
-controller.processGuess("A0");
-controller.processGuess("A6");
-controller.processGuess("B6");
-controller.processGuess("C6");
-controller.processGuess("C4");
-controller.processGuess("D4");
-controller.processGuess("E4");
-controller.processGuess("B0");
-controller.processGuess("B1");
-controller.processGuess("B2");
