c64c8f048d2445ff4793607f999bd61289c38ed2
Jonas <joshuamoorhead@gmail.com>
2016-04-14 10:12:27 -0700

    add model

--- headFirstJS/Chapter8/app.js
+++ headFirstJS/Chapter8/app.js
@@ -12,6 +12,16 @@ var view = {
 		cell.setAttribute("class", "miss");
 	}
 };
+var model = {
+	boardSize: 7,
+	numShips: 3,
+	shipLength: 3,
+	shipsSunk: 0,
+
+	ships: [{ locations: ["06","16","26"], hits: ["","",""] },
+					{ locations: ["24","34","44"], hits: ["","",""] },
+					{ locations: ["10","11","12"], hits: ["","",""] }]
+};
 view.displayMiss("00");
 view.displayHit("34");
 view.displayMiss("55");
